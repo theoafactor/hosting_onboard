7e2edf6f6d52522461f6234025818c13fc57f513
root <root@astronaut>
2026-02-26 11:12:12 +0100

    Added functions

--- mail.sh
+++ mail.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 
-sendmail theoafactor@gmail.com << EOF
-To: theoafactor@gmail.com
-From: theoafactor@gmail.com
-Subject: Test Email
+# sendmail theoafactor@gmail.com << EOF
+# To: theoafactor@gmail.com
+# From: theoafactor@gmail.com
+# Subject: Test Email
 
-This is a test email sent from the script
-EOF
+# This is a test email sent from the script
+# EOF
+
+
+check_user(){
+    echo "Working"
+    echo "Welcome $1"
+
+    return 2
+}
+
+check_user james
+echo $?
+ 
 
--- onboard.sh
+++ onboard.sh
@@ -1,10 +1,43 @@
 #!/bin/bash
 
+## functions 
+create_user(){
+    username=$1
+
+    if id "$username" &> /dev/null; then
+        return 1
+    else 
+        sudo useradd $username
+        sudo mkdir /home/$username
+        sudo chown $username:$username /home/$username
+        password=$( pwgen -B -1 )
+        sudo echo "$username:$password" | sudo chpasswd
+
+        return $password
+    fi
+}
+
+
+send_email(){
+    sendmail $1 << EOF
+To: $1
+From: Cyclobold Hosting <theoafactor@gmail.com>
+Subject: $2
+
+Your hosting details are as follows:
+Username: $3
+RAM: $4 GB
+Storage: $5 GB  
+Password: $6     
+EOF
+}
+
+
 # Function to onboard a new user
 while read line; 
 do
     # Split the line into fields    
-    if [[ $line != "SN,FirstName,LastName,AccountType,RAM,Storage,UserName,Status" ]]
+    if [[ $line != "SN,FirstName,LastName,AccountType,RAM,Storage,UserName,Status,Email" ]]
     then 
         sn=$(echo $line | cut -d',' -f1)
         firstname=$(echo $line | cut -d',' -f2)
@@ -14,32 +47,30 @@ do
         storage=$(echo $line | cut -d',' -f6)
         username=$(echo $line | cut -d',' -f7)
         status=$(echo $line | cut -d',' -f8)
+        email=$(echo $line | cut -d',' -f9)
 
        
         if [[ $accountType == "Shared" ]]
         then 
             ## create shared account 
-            sudo useradd $username
-            sudo mkdir /home/$username
-            sudo chown $username:$username /home/$username
-            password=$( pwgen -B -1 )
-            sudo echo "$username:$password" | sudo chpasswd
+            create_user $username 
+            result=$( echo $? )
+            if [[ $result -eq 1 ]]
+            then
+                echo "Account exists already..."
+                echo "Moving on ..."
 
-            sendmail theoafactor@gmail.com << EOF
-To: theoafactor@gmail.com
-From: Cyclobold Hosting <theoafactor@gmail.com>
-Subject: Your Hosting Details
+            else
 
-Your hosting details are as follows:
-Username: $username
-RAM: $ram GB
-Storage: $storage GB  
-Password: $password      
-EOF
-
+                echo "Account created .."
+                ## send email 
+                send_email $email "Shared Account Created" $username $ram $storage $result
+                
+                sudo echo "User $username created successfully" >> /var/log/onboard.log
+            fi
             
-
-            sudo echo "User $username created successfully" >> /var/log/onboard.log
+        
+            
 
         elif [[ $accountType == "Dedicated" ]]
         then
